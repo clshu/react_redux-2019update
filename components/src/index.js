e12d74708551ece502537310d9bf5a286194b6a6
clshu <ccss0630@yahoo.com>
2019-10-13 07:02:21 -0700

    Create ApprovalCard with Semantic UI view 'card'. Wrap CommentDetail inside ApprovalCard to become one of a props.children. ApprovalCard insert props.children in div className context

--- components/src/index.js
+++ components/src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom'
 import faker from 'faker'
 
 import CommentDetail from './components/CommentDetail'
+import ApprovalCard from './components/ApprovalCard'
 
 const App = () => {
     return (
     <div className="ui container comments">
-        <CommentDetail author="Sam" timeBlog="Today 2:00 pm" text="Hello Word" avatar={faker.image.avatar()}/>
-        <CommentDetail author="Alex" timeBlog="Today 4:00 pm" text="Happy Day" avatar={faker.image.avatar()}/>
-        <CommentDetail author="Bob" timeBlog="Yesterday 2:00 pm" text="Ok Ok" avatar={faker.image.avatar()}/>
+        <ApprovalCard>
+            <CommentDetail author="Sam" timeBlog="Today 2:00 pm" text="Hello Word" avatar={faker.image.avatar()}/>
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail author="Alex" timeBlog="Today 4:00 pm" text="Happy Day" avatar={faker.image.avatar()}/>
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail author="Bob" timeBlog="Yesterday 2:00 pm" text="Ok Ok" avatar={faker.image.avatar()}/>
+        </ApprovalCard>    
     </div>
     )
 }
