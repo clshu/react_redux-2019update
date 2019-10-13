0c640a2aad9512aacb15ffbcaac38449e45be411
clshu <ccss0630@yahoo.com>
2019-10-13 05:53:15 -0700

    Eextract JSX component CommonDetail

--- components/src/index.js
+++ components/src/index.js
@@ -1,53 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import faker from 'faker'
+
+import CommentDetail from './components/CommentDetail'
 
 const App = () => {
     return (
     <div className="ui container comments">
-        <div className="comment">
-            <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()}/>
-            </a>
-            <div className="content">
-                <a href="/" className="author">
-                    Sam
-                </a>
-                <div className="metadata">
-                    <span className="date">Todat 6:00 pm</span>
-                </div>
-                <div className="text">Nice blog post</div>
-            </div>
-        </div>
-        <div className="comment">
-            <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()}/>
-            </a>
-            <div className="content">
-                <a href="/" className="author">
-                    Sam
-                </a>
-                <div className="metadata">
-                    <span className="date">Todat 6:00 pm</span>
-                </div>
-                <div className="text">Nice blog post</div>
-            </div>
-        </div>
-        <div className="comment">
-            <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()}/>
-            </a>
-            <div className="content">
-                <a href="/" className="author">
-                    Sam
-                </a>
-                <div className="metadata">
-                    <span className="date">Todat 6:00 pm</span>
-                </div>
-                <div className="text">Nice blog post</div>
-            </div>
-        </div>
-
+        <CommentDetail />
+        <CommentDetail />
+        <CommentDetail />
     </div>
     )
 }
