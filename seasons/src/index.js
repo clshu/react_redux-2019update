e80ec349a26d98bf06c7cb5aa0f33b639b559f93
clshu <ccss0630@yahoo.com>
2019-10-13 08:31:29 -0700

    Refactor the functional component to a class component. Use state to initialize the state value, then call setState() to to update state and rerender the page.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (postion) => console.log(postion),
-        err => console.log(err)
-    )
-    return (
-        <div></div>
-    )
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {latitude: 0, longitude: 0}
+        
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const {latitude, longitude} = position.coords
+                this.setState({latitude, longitude})
+            },
+            err => console.log(err)
+        )
+    }
+    render() {
+        return (
+            <div>
+                <p>Latitude:{this.state.latitude}</p>
+                <p>Longitude:{this.state.longitude}</p>
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(
