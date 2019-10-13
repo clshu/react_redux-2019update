fc2a9faca75ba34c87b3d0ff897108ffc2a21679
clshu <ccss0630@yahoo.com>
2019-10-13 04:27:44 -0700

    First app, fix style descrpency between JSX and HTML

---
+++ jsx/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <div>
+            <label class="label" for="name">Enter name:</label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+)
